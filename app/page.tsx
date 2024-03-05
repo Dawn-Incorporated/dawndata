8cb69eda1b63fa99b38fe0e38183af9993f79c1c
matisbyar <matis.byar.06@icloud.com>
2024-03-05 15:55:31 +0100

    Retrieve data from homepage (beta)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 import useSWR from 'swr'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Link from "next/link";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json())
 
-export default async function Home() {
+export default function Home() {
     const {data, error} = useSWR('/api/databases', fetcher)
 
     if (error) return <div>Failed to load</div>
@@ -10,7 +14,26 @@ export default async function Home() {
 
     return (
         <div>
-            <h1>{ data }</h1>
+            <h1>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableHeader>Database</TableHeader>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        { data.map((item: any, index: any) => (
+                            <TableRow key={ index }>
+                                <TableCell>
+                                    <Link href={ `/databases/${ item.Database }` }>
+                                        { item.Database }
+                                    </Link>
+                                </TableCell>
+                            </TableRow>
+                        )) }
+                    </TableBody>
+                </Table>
+            </h1>
         </div>
     )
 }
